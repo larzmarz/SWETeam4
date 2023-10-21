d984e87f7bcbc1552908f04aa608e8e2c50c0221
Kevin Camacho <Kevincxv@outlook.com>
2023-10-21 11:22:35 -0400

    Updated URI

--- Shopping Cart/Feature-3.py
+++ Shopping Cart/Feature-3.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
-MONGODB_URI = config('MONGODB_URI')
+MONGODB_URI = 'mongodb+srv://SWE4:SWET4@cluster0.ln7pqzu.mongodb.net/geek_text_db'
 client = MongoClient(MONGODB_URI)
 db = client['geek_text_db']
 # <collection name>_collection = db['<collection name>']
