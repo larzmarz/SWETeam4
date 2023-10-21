7c7f909ffb453ccaf1069573930edccda0f65e34
Kevin Camacho <Kevincxv@outlook.com>
2023-10-21 10:46:07 -0400

    Added gitignore and .env

--- Database.py
+++ Database.py
@@ -1,9 +1,10 @@
+from decouple import config
 from pymongo import MongoClient, DESCENDING
 from flask import Flask, jsonify, request, abort
 
 class DatabaseWrapper:
     def __init__(self):
-        MONGODB_URI = 'mongodb+srv://SWE4:SWET4@cluster0.ln7pqzu.mongodb.net/geek_text_db'
+        MONGODB_URI = config('MONGODB_URI')
         self.client = MongoClient(MONGODB_URI)
         self.db = self.client['geek_text_db']
 
